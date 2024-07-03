c72ffcf84d6dd6f295f33448544bcd991b38b6ab
Ильфат Низамов <ilfatnizamov@MacBook-Air-Ilfat.local>
2024-07-03 17:34:26 +0500

    122 lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
+import  {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World</h2>
+}
+
+// const Field = () => {
+//   const holder  = 'Enter here'
+//   const styleField = {width: '300px'}
+//   return <input placeholder= {holder} type="text" style ={styleField} />
+// }
+
+class Field extends Component {
+  render() {
+    const holder  = 'Enter here'
+    const styleField = {width: '300px'}
+  
+  return <input placeholder={holder} type="text" style ={styleField} />
+  }
+}
+
+function Btn() {
+  const text = "log in"
+  const logged = false
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
-const text = "Hello"
- const elem =  (
-  <div>
-    <h2>{text} </h2>
-    <input type="text" />
-    <button/>
-    </div>
- )
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <div>
+    <App/>
+  </div> 
 );
